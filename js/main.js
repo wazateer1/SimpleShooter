e8c7e872305b670ea0d238dfcffa5722797ba9f9
matt l <wazateer1@gmail.com>
2017-05-16 20:39:24 -0400

    - Added pointerlock and mouse controls

--- js/main.js
+++ js/main.js
@@ -10,12 +10,54 @@ document.body.appendChild(renderer.domElement);
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 10000);
-camera.position.y = 10;
-camera.position.z = 20;
-camera.rotation.x = -0.5;
-scene.add(camera);
+//Check pointers and stuff
+var blocker = document.getElementById('blocker');
+var instructions = document.getElementById('instructions');
 
+var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+if (havePointerLock) {
+	var element = document.body;
+	var pointerlockchange = function (event) {
+		if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+			controlsEnabled = true;
+			controls.enabled = true;
+			blocker.style.display = 'none';
+		} else {
+			controls.enabled = false;
+			blocker.style.display = '-webkit-box';
+			blocker.style.display = '-moz-box';
+			blocker.style.display = 'box';
+			instructions.style.display = '';
+		}
+	};
+	var pointerlockerror = function (event) {
+		instructions.style.display = '';
+	};
+	// Hook pointer lock state change events
+	document.addEventListener('pointerlockchange', pointerlockchange, false);
+	document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+	document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+	document.addEventListener('pointerlockerror', pointerlockerror, false);
+	document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+	document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+	instructions.addEventListener('click', function (event) {
+		instructions.style.display = 'none';
+		// Ask the browser to lock the pointer
+		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+		element.requestPointerLock();
+	}, false);
+} else {
+	instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+}
+
+//Camera + Control (thx Mr Doob)
+var camera = new THREE.PerspectiveCamera(50, WIDTH / HEIGHT, 0.1, 10000);
+var controls = new THREE.PointerLockControls(camera);
+controls.getObject().position.y = 3;
+controls.getObject().position.z = 20;
+scene.add(controls.getObject());
+
+//Lights
 var light1 = new THREE.PointLight(0xffffff, 1.1);
 light1.position.set(-15, 10, -20);
 scene.add(light1);
@@ -27,7 +69,7 @@ light3.position.set(15, 10, 20);
 scene.add(light3);
 
 function generateBattleground() {
-	var obstCount = Math.round(Math.random() * 3 + 2), //Random obstacle count between one and three
+	var obstCount = Math.round(Math.random() * 2 + 3), //Random obstacle count between three and five
 		obstList = ["cube", "sphere", "cone", "cylinder", "octahedron"], //Five available shapes for obstacles
 		obstGrid = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4"]; //12 possible locations (letters being columns parallel to spawning viewpoint)
 
@@ -127,11 +169,98 @@ function loadBattlegroundGeometry(bg) {
 	return bgGroup;
 }
 
-scene.add(loadBattlegroundGeometry(generateBattleground()));
+//Control variables
+var controlsEnabled = false;
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+
+function initKeyControls() {
+	var onKeyDown = function (event) {
+		switch (event.keyCode) {
+			case 38: // up
+			case 87: // w
+				moveForward = true;
+				break;
+			case 37: // left
+			case 65: // a
+				moveLeft = true;
+				break;
+			case 40: // down
+			case 83: // s
+				moveBackward = true;
+				break;
+			case 39: // right
+			case 68: // d
+				moveRight = true;
+				break;
+			case 32: // space
+				if (canJump === true) velocity.y += 7;
+				canJump = false;
+				break;
+		}
+	};
+	var onKeyUp = function (event) {
+		switch (event.keyCode) {
+			case 38: // up
+			case 87: // w
+				moveForward = false;
+				break;
+			case 37: // left
+			case 65: // a
+				moveLeft = false;
+				break;
+			case 40: // down
+			case 83: // s
+				moveBackward = false;
+				break;
+			case 39: // right
+			case 68: // d
+				moveRight = false;
+				break;
+		}
+	};
+	document.addEventListener('keydown', onKeyDown, false);
+	document.addEventListener('keyup', onKeyUp, false);
+}
+initKeyControls();
 
 function render() {
 	requestAnimationFrame(render);
 
+	if (controlsEnabled) {
+		var time = performance.now();
+		var delta = (time - prevTime) / 1000;
+
+		velocity.x -= velocity.x * 10.0 * delta;
+		velocity.z -= velocity.z * 10.0 * delta;
+
+		velocity.y -= 9.8 * 1.0 * delta; //100.0 = mass
+
+		if (moveForward) velocity.z -= 200.0 * delta;
+		if (moveBackward) velocity.z += 200.0 * delta;
+		if (moveLeft) velocity.x -= 200.0 * delta;
+		if (moveRight) velocity.x += 200.0 * delta;
+
+		controls.getObject().translateX(velocity.x * delta);
+		controls.getObject().translateY(velocity.y * delta);
+		controls.getObject().translateZ(velocity.z * delta);
+
+		if (controls.getObject().position.y < 3) {
+			velocity.y = 0;
+			controls.getObject().position.y = 3;
+			canJump = true;
+		}
+
+		prevTime = time;
+	}
 	renderer.render(scene, camera);
 }
+
+scene.add(loadBattlegroundGeometry(generateBattleground()));
+
 render();
